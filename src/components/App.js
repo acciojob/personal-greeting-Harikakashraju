418cfaa0b5cf220e2e341c3d4b2e5b3f6d8fecef
Harikakashraju <125797836+Harikakashraju@users.noreply.github.com>
2023-07-14 18:35:54 +0000

    greeting

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [name, setName] = useState("")
+  const [greeting,setGreeting]=useState("")
+  const [exclmation , setExclamation] = useState("")
+  function hello(event){
+    setGreeting("Hello")
+    setName(event.target.value)
+    setExclamation("!")
+  }
   return (
-    <div>
+    <div className="display">
         {/* Do not remove the main div */}
+        <label for="name">Enter your name:</label><br/>
+        <input type="text" onChange={hello}/>
+        <h1>{greeting} {name}{exclmation}</h1>
     </div>
   )
 }
